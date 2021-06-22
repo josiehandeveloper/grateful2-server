433ed7020c3275b06e8c25f2b02cfdc822be69cc
Josephine Han <jsphnhan@gmail.com>
2021-06-21 17:59:02 -0700

    got rid of likes in the posts

--- migrations/001.do.create_posts.sql
+++ migrations/001.do.create_posts.sql
@@ -1,6 +1,5 @@
 CREATE TABLE posts (
     id SERIAL PRIMARY KEY,
     content TEXT NOT NULL, 
-    date_created TIMESTAMPTZ DEFAULT now() NOT NULL,
-    likes INTEGER NOT NULL default 1
+    date_created TIMESTAMPTZ DEFAULT now() NOT NULL
 );
